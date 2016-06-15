3635f1539c963dd3623a0cd61ff31d4bfa15772c
vini-goulart <vini@magnetoelenco.com.br>
2016-06-15 19:05:01 -0300

    Correções no index

--- index.php
+++ index.php
@@ -3,135 +3,83 @@
 <head>
 	<meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no">
-
-	<title>Magneto Elenco </title>
+	<title>Magneto Elenco - Agência de Atores, modelos e talentos.</title>
 	<link rel="stylesheet" href="css/swiper.min.css">
 	<link rel="stylesheet" href="css/style.css">
-    <link rel="stylesheet" href="css/bootstrap.min.css">
-
 </head>
 <body>
-
-<div class="swiper-container">
-    <!-- Additional required wrapper -->
-    <div class="swiper-wrapper">
-        <!-- Slides -->
-        <div class="swiper-slide" id="gradient">
-<div class="logo">
-    <img src="images/logo.svg" alt="logo">
-</div>
-
-<div class="slide1">
-        <div class="container">
-
-            <div class="row">
-                <div class="col-md-4 col-sm-6 col-xs-12">
-                    <h1>Prazer!</h1>
+<div class="swiper-container"><!-- Additional required wrapper -->
+    <div class="swiper-wrapper"><!-- Slides -->
+        <div class="swiper-slide" id="gradient"><!-- Slide 1 -->
+            <div class="slide1">
+                <div class="index__body-container">
+                    <div class="index__sub-container-right">
+                        <div class="index__title-text-block">
+                            
+                            <div class="index__text">Somos uma agência de oportunidades!</div>
+                            <div class="index__title">Prazer!</div>
+                        </div>
+                    </div>
+                    <div class="index__sub-container-left">
+                        <div class="logo"><img src="images/logo.svg"></div>
+                    </div>
+                </div>
+                <div class="index__footer"><a href="form_2.php"><footer>Pular Introdução</footer></a></div>
+            </div>
+        </div><!-- Fim Slide 1 -->
+        <div class="swiper-slide" id="gradient2"><!-- Slide 2 -->
+            <div class="slide2">
+                <div class="index__body-container">
+                    <div class="index__sub-container-right">
+                        <div class="index__title-text-block">
+                            
+                            <div class="index__text">Para atores e modelos de todos os perfis</div>
+                        </div>
+                    </div>
+                    <div class="index__sub-container-left">
+                        <div class="logo"><img src="images/logo.svg"></div>
+                    </div>
+                </div>
+                <div class="index__footer"><a href="form_2.php"><footer>Pular Introdução</footer></a></div>
+            </div>
+        </div><!-- Fim Slide 2 -->
+        <div class="swiper-slide" id="gradient3"><!-- Slide 3 -->
+            <div class="slide3">
+                <div class="index__body-container">
+                    <div class="index__sub-container-right">
+                        <div class="index__title-text-block">
+                            
+                            <div class="index__text">Conectando pessoas a anunciantes</div>
+                        </div>
+                    </div>
+                    <div class="index__sub-container-left">
+                        <div class="logo"><img src="images/logo.svg"></div>
+                    </div>
+                </div>
+                <div class="index__footer"><a href="form_2.php"><footer>Pular Introdução</footer></a></div>
+            </div>
+        </div><!-- Fim Slide 3 -->        
+        <div class="swiper-slide" id="gradient4"><!-- Slide 4 -->
+            <div class="slide4">
+                <div class="index__body-container">
+                    <div class="index__sub-container-right">
+                        <div class="index__title-text-block">
+                            <div class="index__start-btn btn btn--primary btn--medium">Iniciar Cadastro</div>
+                            <div class="index__text">Faça parte do nosso elenco!</div>
+                        </div>
+                    </div>
+                    <div class="index__sub-container-left">
+                        <div class="logo"><img src="images/logo.svg"></div>
+                    </div>
                 </div>
             </div>
-            <div class="row">
-                <div class="col-md-4 col-sm-6 col-xs-12">
-                    <p>Somos uma  <br>  agência de <br> oportunidades!</p>
-                </div>
-            </div>
-        </div>
+        </div><!-- Fim Slide 4 -->  
+<div class="swiper-pagination"></div><!-- bolinhas -->
 </div>
-        <footer class="footer">
-            <a href="index2.php">Pular Introdução</a>  
-        </footer>
-        
-        </div>
-
-        <div class="swiper-slide"  id="gradient2">
-
-<div class="logo">
-    <img src="images/logo.svg" alt="logo">
 </div>
-
-<div class="slide2">
-         <div class="container">
-         <div class="row">
-                <div class="col-md-4 col-sm-6 col-xs-12">
-                    <p>Para atores e <br /> modelos de <br> todos os perfis</p>
-                </div>
-            </div>
-
-        </div>
-</div>
-        <footer class="footer">
-            <a href="index2.php">Pular Introdução</a>  
-        </footer>
-        
-        </div>
-
-        <div class="swiper-slide"  id="gradient3">
-
-<div class="logo">
-    <img src="images/logo.svg" alt="logo">
-</div>
-
-<div class="slide2">
-         <div class="container">
-         <div class="row">
-                <div class="col-md-4 col-sm-6 col-xs-12">
-                   <p>Conectando<br /> pessoas a <br> anunciantes</p>
-                </div>
-            </div>
-
-        </div>
-</div>
-
-        
-        <footer class="footer">
-            <a href="index2.php">Pular Introdução</a>  
-        </footer>
-        
-        </div>
-        
-<div class="swiper-slide"  id="gradient4">
-
-<div class="logo">
-    <img src="images/logo.svg" alt="logo">
-</div>
-
-<div class="slide4">
-         <div class="container">
-         <div class="row">
-                <div class="col-md-4 col-sm-6 col-xs-12 slide4p">
-                   <p class="slide4p2">Faça parte do <br> nosso elenco!</p>
-                </div>
-         </div>
-         <div class="row">
-                <div class="col-md-4 col-sm-6 col-xs-12">
-                   
-                       <a href="index2.php" class="btn">
-                           Iniciar
-                        </a>    
-                        <span>
-                             <img src="images/arrow-right.svg" alt="iniciar seta">
-                        </span>        
-                    
-                </div>
-         </div>
-
-        </div>
-</div>
-
-</div>
-
-    </div>
-    <!-- bolinhas -->
-    <div class="swiper-pagination"></div>
-    
-</div>
-	
-
-
 <script src="js/jquery-1.12.1.min.js"></script>
 <script src="js/swiper.jquery.min.js"></script>
 <script src="js/swiper.min.js"></script>
 <script src="js/script.js"></script>
-<script src="js/bootstrap.min.js"></script>
-
 </body>
 </html>
