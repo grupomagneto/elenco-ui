bae82cc848c18829f02decd7fc29f6cfcce4fb99
kapcruz <kapcruz@gmail.com>
2016-07-18 18:46:07 -0300

    Modificações modal agenda

--- agenda/agendamento/source/assets/javascripts/modal.js
+++ agenda/agendamento/source/assets/javascripts/modal.js
@@ -1,5 +1,18 @@
 $(document).ready(function(){
-	$('.button-modal-agenda').click(function(){
+	$('.button-modal-agenda__premium').click(function(){
 		$('#aviso-premium').modal('hide');
 	}); 
 });
+
+$(document).ready(function(){
+	$('.button-modal-agenda__gratuito').click(function(){
+		$('#aviso-gratuito').modal('hide');
+	}); 
+});
+
+$(document).ready(function(){
+	$('.button-modal-agenda__profissional').click(function(){
+		$('#aviso-profissional').modal('hide');
+	}); 
+});
+
