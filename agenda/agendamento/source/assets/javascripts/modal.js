549fec5f1e94108602c71dd5649b949e169cad04
kapcruz <kapcruz@gmail.com>
2016-07-20 13:25:48 -0300

    primeiro commit agenda com método bem

--- agenda/agendamento/source/assets/javascripts/modal.js
+++ agenda/agendamento/source/assets/javascripts/modal.js
@@ -1,18 +1,33 @@
-$(document).ready(function(){
-	$('.button-modal-agenda__premium').click(function(){
-		$('#aviso-premium').modal('hide');
-	}); 
+$(".button-modal-agenda__premium").click(function() {
+  $('#aviso-premium').modal('hide');
 });
 
-$(document).ready(function(){
-	$('.button-modal-agenda__gratuito').click(function(){
-		$('#aviso-gratuito').modal('hide');
-	}); 
+$(".button-modal-agenda__premium").click(function() {
+  $('#aviso-gratuito').modal('hide');
 });
 
-$(document).ready(function(){
-	$('.button-modal-agenda__profissional').click(function(){
-		$('#aviso-profissional').modal('hide');
-	}); 
+$(".button-modal-agenda__profissional").click(function() {
+  $('#aviso-profissional').modal('hide');
 });
 
+$(document).ready(function () {
+
+    $("#for_a").click(function () {
+        $("#item-one").animate({
+            marginLeft: "-1600px"
+        }, 1000)
+
+        $("#item-two").animate({
+            marginLeft: "0px"
+        }, 1000)
+    })
+
+    $("#for_b").click(function () {
+        $("#item-two").animate({
+            marginLeft: "1600px"
+        }, 1000)
+        $("#item-one").animate({
+            marginLeft: "0px"
+        }, 1000)
+    })
+})
