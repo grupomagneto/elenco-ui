e91fe6c173ac82f8b3bb4089202582765231ffc6
kapcruz <kapcruz@gmail.com>
2016-08-11 18:11:28 -0300

    Primeiro commit api facebook
    faltando dados de localização

---
+++ facebook-login/config.php
@@ -0,0 +1,40 @@
+<?php
+session_start();
+
+require 'face_login.php';
+require 'facebook-php/autoload.php';
+
+use Facebook\FacebookSession;
+use Facebook\FacebookRedirectLoginHelper;
+use Facebook\FacebookRequest;
+use Facebook\FacebookResponse;
+use Facebook\GraphUser;
+use Facebook\GraphObject;
+use Facebook\GraphLocation;
+use Facebook\FacebookRequestException;
+
+FacebookSession::setDefaultApplication($config['app_id'], $config['app_secret']);
+$helper = new FacebookRedirectLoginHelper('http://localhost:8888/elenco-ui/facebook-login/index.php');
+
+try {
+	$session = $helper->getSessionFromRedirect();
+	if ($session):
+		$_SESSION['facebook'] = $session->getToken();
+		header('Location: index.php');
+	endif;
+	if (isset($_SESSION['facebook'])):
+		$session = new FacebookSession($_SESSION['facebook']);
+		$request = new FacebookRequest($session, 'GET', '/me?fields=id,first_name,last_name,email,gender,birthday');
+		$response = $request->execute();
+		$user = $response->getGraphObject(GraphUser::className());
+		$facebook_user = $user;
+		$id = $user->getId();
+		$img = 'https://graph.facebook.com/'.$id.'/picture?width=300';
+ 		$birthday = $user->getProperty('birthday');
+
+	endif;
+} catch(FacebookRequestException $ex) {
+  // Quando Facebook retorna um erro
+} catch(\Exception $ex) {
+  // Quando a validação falhar o
+}
---
+++ facebook-login/index.php
@@ -0,0 +1,39 @@
+<?php
+	require_once 'config.php';
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Facebook login</title>
+	<!-- Latest compiled and minified CSS -->
+	<link rel="stylesheet" href="//maxcdn.bootstrapcdn.com/bootstrap/3.2.0/css/bootstrap.min.css">
+
+	<style>
+		body {
+			margin: 50px auto;
+			width: 400px;
+			text-align: center;
+		}
+	</style>
+</head>
+<body>
+	<h2>Facebook login</h2>
+
+	<?php if (!isset($_SESSION['facebook'])): ?>
+		<a href="<?php echo $helper->getLoginUrl($config['scopes']); ?>" class="btn btn-primary">Iniciar com Facebook!</a>
+	<?php else: ?>
+		<p>
+			 <img src='<?php echo $img;?>' /> <br />
+			Id: <?php echo $facebook_user->getId(); ?> <br />
+			Primeiro nome: <?php echo $facebook_user->getFirstName(); ?> <br />
+			Último nome: <?php echo $facebook_user->getLastName(); ?> <br />
+			Email: <?php echo $facebook_user->getEmail(); ?> <br />
+			Gênero: <?php echo $facebook_user->getGender(); ?> <br />
+			Data de aniversário: <?php echo $birthday ?> <br />
+
+		</p>
+		<a href="logout.php" class="btn btn-danger">Encerrar sessão</a>
+	<?php endif; ?>
+</body>
+</html>
