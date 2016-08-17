98d751c3c44b5ae526f7a9553a729ba666ab6b39
kapcruz <kapcruz@gmail.com>
2016-08-17 19:06:34 -0300

    modificações mobile jogo

--- facebook-jogo/jogo/source/javascripts/all.js
+++ facebook-jogo/jogo/source/javascripts/all.js
@@ -1,23 +1,21 @@
-var mySwiper = new Swiper ('.swiper-container-h', {
+var mySwiper = new Swiper ('.swiper-container', {
 	// Optional parameters
 	direction: 'horizontal',
 	loop: false,
 	pagination: '.swiper-pagination',
-	paginationClickable: true,
 	keyboardControl: true,
-	speed: 900
+	speed: 200
 })
-
-var mySwiper = new Swiper ('.swiper-container-v', {
-  // Optional parameters
-  direction: 'vertical',
-  loop: false,
-  nextButton: '.swiper-button-next',
-  prevButton: '.swiper-button-prev',
-  paginationClickable: true,
-  keyboardControl: true,
-  speed: 200
-})           
+        
+mySwiper.bullets[4].style.display='none';
+mySwiper.bullets[5].style.display='none';
+mySwiper.bullets[6].style.display='none';
+mySwiper.bullets[7].style.display='none';
+mySwiper.bullets[8].style.display='none';
+mySwiper.bullets[9].style.display='none';
+mySwiper.bullets[10].style.display='none';
+mySwiper.bullets[11].style.display='none';
+mySwiper.bullets[12].style.display='none';
 
 var colors = new Array(
 [165, 0, 200], [176, 116, 255], [255, 41, 129], [237, 107, 107], [201, 87, 222], [35, 188, 237]
@@ -61,6 +59,49 @@ function updateGradient(container) {
   }
 }
 setInterval(function(){updateGradient('.gradient')},10);
+
+var colors = new Array(
+[165, 0, 200], [176, 116, 255], [255, 41, 129], [237, 107, 107], [201, 87, 222], [35, 188, 237]
+);
+
+var step = 0;
+var colorIndices = [0,1,2,3];
+
+var gradientSpeed = 0.001;
+
+function updateGradient(container) {
+  var c0_0 = colors[colorIndices[0]];
+  var c0_1 = colors[colorIndices[1]];
+  var c1_0 = colors[colorIndices[2]];
+  var c1_1 = colors[colorIndices[3]];
+
+  var istep = 1 - step;
+  var r1 = Math.round(istep * c0_0[0] + step * c0_1[0]);
+  var g1 = Math.round(istep * c0_0[1] + step * c0_1[1]);
+  var b1 = Math.round(istep * c0_0[2] + step * c0_1[2]);
+  var color1 = "rgb("+r1+","+g1+","+b1+")";
+
+  var r2 = Math.round(istep * c1_0[0] + step * c1_1[0]);
+  var g2 = Math.round(istep * c1_0[1] + step * c1_1[1]);
+  var b2 = Math.round(istep * c1_0[2] + step * c1_1[2]);
+  var color2 = "rgb("+r2+","+g2+","+b2+")";
+
+  $(container).css({
+    background: "-webkit-gradient(linear, left top, right top, from("+color1+"), to("+color2+"))"}).css({
+    background: "-moz-linear-gradient(left, "+color1+" 0%, "+color2+" 100%)"});
+
+  step += gradientSpeed;
+  if ( step >= 1 ) {
+    step %= 1;
+    colorIndices[0] = colorIndices[1];
+    colorIndices[2] = colorIndices[3];
+
+    colorIndices[1] = ( colorIndices[1] + Math.floor( 1 + Math.random() * (colors.length - 1))) % colors.length;
+    colorIndices[3] = ( colorIndices[3] + Math.floor( 1 + Math.random() * (colors.length - 1))) % colors.length;
+
+  }
+}
+setInterval(function(){updateGradient('.gradient-slide_first')},10);
 
 var colors = new Array(
 [165, 0, 200], [176, 116, 255], [255, 41, 129], [237, 107, 107], [201, 87, 222], [35, 188, 237]
