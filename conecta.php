2da6dd15c5edca6006d3ef0056e63dae53e5e12a
vini-goulart <Vgb808285>
2016-08-25 23:07:33 -0300

    Conecta

--- conecta.php
+++ conecta.php
@@ -7,7 +7,6 @@ $db = 'testecadastro';
 $host = 'localhost';
 $port = 8889;
 
-
 $link = mysqli_init();
 $success = mysqli_real_connect(
    $link,
@@ -17,13 +16,14 @@ $success = mysqli_real_connect(
    $db,
    $port
 );
+mysqli_set_charset($link,"utf8");
 
 // DB Online
 // $user = 'vinigoulart1';
 // $password = 'ThM]HETPv@';
 // $db = 'testecadastro';
 // $host = 'p3plcpnl0612.prod.phx3.secureserver.net';
-//
+
 // $link = mysqli_init();
 // $success = mysqli_real_connect(
 //    $link,
@@ -32,5 +32,21 @@ $success = mysqli_real_connect(
 //    $password,
 //    $db
 // );
-mysqli_set_charset($link,"utf8");
+// mysqli_set_charset($link,"utf8");
+
+// DB Online 2
+$user2 = 'vinigoulart12';
+$password2 = 'm@g3l3nc01122';
+$db2 = 'vinigoulart12';
+$host2 = 'mysql02.vinigoulart.com.br';
+
+$link2 = mysqli_init();
+$success2 = mysqli_real_connect(
+   $link2,
+   $host2,
+   $user2,
+   $password2,
+   $db2
+);
+mysqli_set_charset($link2,"utf8");
 ?>
--- conecta_on.php
+++ conecta_on.php
@@ -7,7 +7,6 @@
 // $host = 'localhost';
 // $port = 8889;
 
-
 // $link = mysqli_init();
 // $success = mysqli_real_connect(
 //    $link,
@@ -17,6 +16,7 @@
 //    $db,
 //    $port
 // );
+// mysqli_set_charset($link,"utf8");
 
 // DB Online
 $user = 'vinigoulart1';
@@ -33,4 +33,20 @@ $success = mysqli_real_connect(
    $db
 );
 mysqli_set_charset($link,"utf8");
+
+// DB Online 2
+$user2 = 'vinigoulart12';
+$password2 = 'm@g3l3nc01122';
+$db2 = 'vinigoulart12';
+$host2 = 'mysql02.vinigoulart.com.br';
+
+$link2 = mysqli_init();
+$success2 = mysqli_real_connect(
+   $link2,
+   $host2,
+   $user2,
+   $password2,
+   $db2
+);
+mysqli_set_charset($link2,"utf8");
 ?>
