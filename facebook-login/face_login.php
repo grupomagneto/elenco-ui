841290480ef27ea22bc3199a663b40231b5ec292
kapcruz <kapcruz@gmail.com>
2016-09-11 13:27:10 -0300

    Correções config api facebook

--- facebook-login/face_login.php
+++ facebook-login/face_login.php
@@ -4,5 +4,5 @@
 $config = array(
   'app_id' => '908603925934775',
   'app_secret' => '404a883e0f0f6dde4f134feeebec8e22',
-  'scopes' => array('scope' => 'email,user_location,user_birthday')
+  'scopes' => array('scope' => 'email, public_profile')
 );
