8e5e8060e13166842bf705a81f3bf8ccc13249f0
kapcruz <kapcruz@gmail.com>
2017-03-02 17:59:39 -0300

    Efeito css dislike

--- perfil-slim/perfil-html/teste.php
+++ perfil-slim/perfil-html/teste.php
@@ -6,13 +6,11 @@
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 	<link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/font-awesome/4.2.0/css/font-awesome.min.css">
 <style>
-
 	*{
 		-webkit-box-sizing: border-box;
 	  	-moz-box-sizing: border-box;
 		box-sizing: border-box;
-		}
-
+	}
 	html, body{
 		background-color:#F2F2F2;
 		font-family:Segoe, "Segoe UI", "DejaVu Sans", "Trebuchet MS", Verdana, sans-serif;
@@ -24,30 +22,26 @@
 		color:#404040;
 		position:relative;
 	}
-
 	a{
 		-webkit-transition: all 0.3s ease;
 	    -moz-transition: all 0.3s ease;
 	    -o-transition: all 0.3s ease;
 	    transition: all 0.3s ease;
 	}
-
 	.wrapper{
-		width:450px;
-		margin:30px auto 0;
-		background-color:#FFFFFF;
+		width: 290px;
+		margin: 30px auto 0;
+		background-color: #FFFFFF;
 		-webkit-box-sizing: border-box;
 	  	-moz-box-sizing: border-box;
 		box-sizing: border-box;
 	}
-
 	header{
 		text-align:right;
 		padding:10px;
 		margin-bottom:10px;
 		background-color:#5DBA9D;
 	}
-
 	header a{
 		font-size:20px;
 		color:#FFFFFF;
@@ -58,78 +52,177 @@
 		text-align:center;
 		display:inline-block;
 	}
-
 	header a:hover, .list-mode header a.hide-list:hover{
-		background-color:#11956c;
+		background-color: #11956c;
 	}
-
 	header a.hide-list{
-		background-color:#11956c;
+		background-color: #11956c;
 	}
-
 	.list-mode header a.hide-list{
 		background-color: #5DBA9D;
 	}
-
 	.list-mode header a.show-list{
 		background-color: #11956c;
 	}
-
 	.list-mode header a.show-list-single{
 		background-color: #ffffff;
 	}
-
 	.container:after{
 		content: "";
 		clear: both;
 		display: table;
 	}
-
 	.container{
-		padding:10px 0 10px 10px;
+		padding: 2px 2px 5px 2px;
 	}
-
 	.wrapper .box{
-		float:left;
-		width: 100px;
-		height: 100px;
-		margin:0 10px 10px 0;
-		background-color:#CCCCCC;
-		-webkit-transition:all 1.0s ease;
-		-moz-transition:all 1.0s ease;
-		transition:all 1.0s ease;
-		transition:all 1.0s ease;
+		float: left;
+		width: 95px;
+		height: 95px;
+		margin: 0 1px 1px 0;
+		background-color: #CCCCCC;
+		-webkit-transition: all 1.0s ease;
+		-moz-transition: all 1.0s ease;
+		transition: all 1.0s ease;
 	}
-
+	.wrapper .box .photo{
+		float: left;
+		width: 95px;
+		height: 95px;
+		margin: 0 1px 1px 0;
+		background-color: #CCCCCC;
+		-webkit-transition: all 1.0s ease;
+		-moz-transition: all 1.0s ease;
+		transition: all 1.0s ease;
+		transition: all 1.0s ease;
+	}
 	.wrapper.list-mode .container{
-		padding-right: 10px;
+		padding-right: 2px;
 	}
-
 	.wrapper.list-mode .box{
+		width: 142px;
+		height: 142px;
+	}
+	.wrapper.list-mode .box .photo{
+		width: 142px;
+		height: 142px;
+	}
+	.wrapper.list-mode-single .container{
+		padding-right: 5px;
+	}
+	.wrapper.list-mode-single .box{
+		width: 100%;
+		position: relative;
+	}
+	.wrapper.list-mode-single .box:nth-child(even) {
+		margin-top: -92px;
+		width: 100%;
+	}
+	.wrapper.list-mode-single .box:nth-child(odd) {
+		margin-top: -92px;
+		width: 100%;
+	}
+	.wrapper.list-mode-single .box:first-of-type {
+		margin-top: 5px;
+		width: 100%;
+	}
+	.wrapper.list-mode-single .box .photo{
+		transition: all 100ms ease-in-out;
+		-webkit-transition: all 100ms ease-in-out;
+		-moz-transition: all 100ms ease-in-out;
+		-ms-transition: all 100ms ease-in-out;
+		-o-transition: all 100ms ease-in-out;
+        -o-box-shadow: 0 2px 15px 1px rgba(225, 225, 225, 0.5);
+        -ms-box-shadow: 0 2px 15px 1px rgba(225, 225, 225, 0.5);
+        -moz-box-shadow: 0 2px 15px 1px rgba(225, 225, 225, 0.5);
+        -webkit-box-shadow: 0 2px 15px 1px rgba(225, 225, 225, 0.5);
+        box-shadow: 0 1px 4px 1px rgba(0, 0, 0, 0.5);
+        border-radius: 2px;
+        position: absolute;
+        list-style: none;
+        height: 100%;
+        left: 0;
+        right: 0;
+        margin: 0 auto;
+        text-align: center;
+	}
+	.wrapper.list-mode-single .box .photo:first-of-type {
+		top: 1px;
 		width: 100%;
 	}
 
-	.wrapper.list-mode-single .container{
-		padding-right:10px;
+	.animated {
+	  -webkit-animation-duration: 1s;
+	  animation-duration: 1s;
+	  -webkit-animation-fill-mode: both;
+	  animation-fill-mode: both;
 	}
 
-	.wrapper.list-mode-single .box{
-		width: 10%;
+	@-webkit-keyframes rotateOutUpLeft {
+	  from {
+	    -webkit-transform-origin: left bottom;
+	    transform-origin: left bottom;
+	  }
+
+	  to {
+	    -webkit-transform-origin: left bottom;
+	    transform-origin: left bottom;
+	    -webkit-transform: rotate3d(0, 0, 1, -45deg);
+	    transform: rotate3d(0, 0, 1, -45deg);
+	  }
 	}
+
+	@keyframes rotateOutUpLeft {
+	  from {
+	    -webkit-transform-origin: left bottom;
+	    transform-origin: left bottom;
+	  }
+
+	  to {
+	    -webkit-transform-origin: left bottom;
+	    transform-origin: left bottom;
+	    -webkit-transform: rotate3d(0, 0, 1, -45deg);
+	    transform: rotate3d(0, 0, 1, -45deg);
+	  }
+	}
+
+	.rotateOutUpLeft {
+	  -webkit-animation-name: rotateOutUpLeft;
+	  animation-name: rotateOutUpLeft;
+	}
+
 </style>
+
 </head>
 <body>
+	
 	<div class="wrapper">
 
 	    <div class="container">
-	    	<div class="box">
+	    	<div class="box animated">
+	    		<div class="photo"></div>
 	    	</div>
-	        <div class="box">
-	        	
+	        <div class="box animated">
+	        	<div class="photo"></div>
+	        </div>
+	        <div class="box animated">
+	        	<div class="photo"></div>
+	        </div>
+	        <div class="box animated">
+	        	<div class="photo"></div>
+	        </div>
+	        <div class="box animated">
+	        	<div class="photo"></div>
+	        </div>
+	        <div class="box animated">
+	        	<div class="photo"></div>
 	        </div>
 	    </div>
 
+		<button type="button" class="dislike" id="dislike">dislike</button>
+		<button type="button" id="like">like</button>
 	</div>
+
  	<header>
     	<a href="javascript:void(0)" class="show-list-single">
     		<i class="fa fa-square-o"></i>
@@ -145,25 +238,32 @@
 
 <script src="//cdnjs.cloudflare.com/ajax/libs/jquery/2.1.3/jquery.min.js"></script>
 <script src="//cdnjs.cloudflare.com/ajax/libs/jqueryui/1.11.2/jquery-ui.min.js"></script>
-<script src="http://codepen.io/WhiteWolfWizard/pen/51307327227e72044ecac64310404cb3.js"></script>
-<script>
-	$('.show-list-single').click(function(){
-		$('.wrapper').removeClass('list-mode');
-		$('.wrapper').addClass('list-mode-single');
-	});
+	<script>
+		$('.show-list-single').click(function(){
+			$('.wrapper').removeClass('list-mode');
+			$('.wrapper').addClass('list-mode-single');
+		});
 
-	$('.show-list').click(function(){
-		$('.wrapper').removeClass('list-mode-single');
-		$('.wrapper').addClass('list-mode');
-	});
+		$('.show-list').click(function(){
+			$('.wrapper').removeClass('list-mode-single');
+			$('.wrapper').addClass('list-mode');
+		});
 
-	$('.hide-list').click(function(){
-		$('.wrapper').removeClass('list-mode-single');
-		$('.wrapper').removeClass('list-mode');
-	});
+		$('.hide-list').click(function(){
+			$('.wrapper').removeClass('list-mode-single');
+			$('.wrapper').removeClass('list-mode');
+		});
 
-</script>
+		$(".dislike").click(function(){
+		    $(".wrapper.list-mode-single.box:first-child").toggleClass('rotateOutUpLeft');
+		});
 
+
+		// $('#dislike').click(function (e) {
+  //   		$(".wrapper.list-mode-single.box.photo:first-of-type").addClass('rotateOutUpLeft');
+		// });
+
+	</script>
 
 </body>
 </html>
