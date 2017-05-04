2d81891e0243f5d47fb31a489462642ff02a3ee7
kapcruz <kapcruz@gmail.com>
2017-05-04 18:16:26 -0300

    Update

--- perfil-slim/perfil-html/javascripts/ajax.js
+++ perfil-slim/perfil-html/javascripts/ajax.js
@@ -16,7 +16,88 @@ $(".botaofavorita").click(function(){
   });
 }); 
 
-$('.checkbox-image-action__fav').on('click', function() {
+//$('.checkbox-image-action__fav').on('click touchstart', function() {
+//    
+//    jQuery('form').submit(function(){
+//    var dados = jQuery( this ).serialize();
+//    
+//    jQuery.ajax({
+//      type: "POST",
+//      dataType: 'html',
+//      url: "api/single_profile.php",
+//      data: dados,
+//      success: function( data )
+//      { 
+//				
+//        $(".gradient").css('display', 'none');
+//        $(".photo__single").css('display', 'block');
+//        $(".photo__single").html(data);
+//        $(".photo__single").load("../api/single_profile.php");
+//        $('.container-outline__single').css('display', 'block');
+//        $('.container-outline__categories').css('display', 'block');
+//        $('.container').css('display', 'none');
+//         
+//        $(".close").click(function() {
+//            $(".photo__single").css("display", "none");
+//            $(".container-outline__single").css("display", "none");
+//            $(".container-outline__categories").css("display", "none");
+//            $(".gradient").css("display", "block");
+//            $(".container").css("display", "block");
+//        });
+//          
+//        var mySwiper = new Swiper ('.swiper-container', {
+//            // Optional parameters
+//            direction: 'horizontal',
+//            loop: false,
+//            keyboardControl: true,
+//            // If we need pagination
+//            pagination: '.swiper-pagination'
+//        });
+//          
+//          
+//      }
+//    });
+//    return false;
+//  });
+//});
+
+//double click actions
+
+//$(document).ready(function(){
+//    $("img.tab-image__background").dblclick(function(){
+//         event.preventDefault();
+//         var $input = $("input[name=imagefavorita]")
+//         $input.prop('checked', true);
+//    });
+//});
+
+
+if( /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent) ) {
+	
+alert("mobile detected");
+ 
+jQuery.fn.single_double_click = function(single_click_callback, double_click_callback, timeout) {
+    return this.each(function() {
+        var clicks = 0,
+            self = this;
+        jQuery(this).click(function(event) {
+            clicks++;
+            if (clicks == 1) {
+                setTimeout(function() {
+                    if (clicks == 1) {
+                        single_click_callback.call(self, event);
+                    } else {
+                        double_click_callback.call(self, event);
+                    }
+                    clicks = 0;
+                }, timeout || 300);
+            }
+    				return false;
+        });
+    });
+}
+
+$(".checkbox-image-action__fav").single_double_click(function (event) {
     
     jQuery('form').submit(function(){
     var dados = jQuery( this ).serialize();
@@ -28,6 +109,7 @@ $('.checkbox-image-action__fav').on('click', function() {
       data: dados,
       success: function( data )
       { 
+				
         $(".gradient").css('display', 'none');
         $(".photo__single").css('display', 'block');
         $(".photo__single").html(data);
@@ -58,152 +140,23 @@ $('.checkbox-image-action__fav').on('click', function() {
     });
     return false;
   });
+
+
+	
+}, function (event) { 
+	
+   alert("dblclick"); 
+	
+	 event.preventDefault();
+	
+	 var $input = $("input[name=imagefavorita]")
+	 $input.prop('checked', true);
+	
+	$('img.fav-action', $(this).closest(".tab-actions__multiples")).fadeToggle('fast');
 });
-
-//double click actions
-$(document).ready(function(){
-    $("img.tab-image__background").dblclick(function(){
-         event.preventDefault();
-			    $(this).toggleClass('checkbox-image-action__fav');
-			
-         var $input = $("input[name=imagefavorita]")
-         $input.prop('checked', true);
-			
-
-			jQuery('form').submit(function(){
-			var dados = jQuery( this ).serialize();
-
-			jQuery.ajax({
-				type: "POST",
-				dataType: 'html',
-				url: "api/addsearch.php",
-				data: dados,
-				success: function( data )
-				{ 
-
-					$(".photo__single").css("display", "none");
-					$(".container-outline__single").css("display", "none");
-					$(".container-outline__categories").css("display", "none");
-					$(".gradient").css("display", "block");
-					$(".container").css("display", "block");
-
-				}
-			});
-			return false;
-		});
-			
-			
-  });
-});
-
-
-//if( /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent) ) {
-	//	
-	//alert("mobile detected");
-	//	
-	//
-	//jQuery.fn.single_double_click = function(single_click_callback, double_click_callback, timeout) {
-	//    return this.each(function() {
-	//        var clicks = 0,
-	//            self = this;
-	//        jQuery(this).click(function(event) {
-	//            clicks++;
-	//            if (clicks == 1) {
-	//                setTimeout(function() {
-	//                    if (clicks == 1) {
-	//                        single_click_callback.call(self, event);
-	//                    } else {
-	//                        double_click_callback.call(self, event);
-	//                    }
-	//                    clicks = 0;
-	//                }, timeout || 250);
-	//            }
-	//        });
-	//    });
-	//}
-	//
-	//$(".checkbox-image-action__fav").single_double_click(function (event) {
-	//    
-	//    alert("click disparado");
-	//	
-	//    jQuery('form').submit(function(){
-	//    var dados = jQuery( this ).serialize();
-	//    
-	//    jQuery.ajax({
-	//      type: "POST",
-	//      dataType: 'html',
-	//      url: "api/single_profile.php",
-	//      data: dados,
-	//      success: function( data )
-	//      {		
-	//				
-	//        $(".gradient").css('display', 'none');
-	//        $(".photo__single").css('display', 'block');
-	//        $(".photo__single").html(data);
-	//        $(".photo__single").load("../api/single_profile.php");
-	//        $('.container-outline__single').css('display', 'block');
-	//        $('.container-outline__categories').css('display', 'block');
-	//        $('.container').css('display', 'none');
-	//         
-	//        $(".close").click(function() {
-	//            $(".photo__single").css("display", "none");
-	//            $(".container-outline__single").css("display", "none");
-	//            $(".container-outline__categories").css("display", "none");
-	//            $(".gradient").css("display", "block");
-	//            $(".container").css("display", "block");
-	//        });
-	//          
-	//        var mySwiper = new Swiper ('.swiper-container', {
-	//            direction: 'horizontal',
-	//            loop: false,
-	//            keyboardControl: true,
-	//            pagination: '.swiper-pagination'
-	//        });
-	//          
-	//      }
-	//    });
-	//      return false;
-	//  });
-	//
-	//
-	//	
-	//}, function (event) { 
-	//  alert("dblclick disparado"); 
-	//	
-	//	jQuery('form').submit(function(){
-	//    var dados = jQuery( this ).serialize();
-	//    
-	//    jQuery.ajax({
-	//      type: "POST",
-	//      dataType: 'html',
-	//      url: "api/addsearch.php",
-	//      data: dados,
-	//      success: function( data )
-	//      {
-	//				
-	//				alert("ajax dblclick sucesso!!!");
-	//				
-	//				$(".photo__single").css("display", "none");
-	//				$(".container-outline__single").css("display", "none");
-	//				$(".container-outline__categories").css("display", "none");
-	//				$(".gradient").css("display", "block");
-	//				$(".container").css("display", "block");
-	//          
-	//      }
-	//    });
-	// 			return false;
-	//  });
-	//	
-	// $('img.fav-action', $(this).closest(".tab-    actions__multiples")).fadeToggle('fast');
-	//	
-	// var $input = $("input[name=imagefavorita]")
-	// $input.prop('checked', true);
-	//
-	//});
-	//	
-	//	
-//}
-
+	
+	
+}
 
 
 
@@ -243,4 +196,3 @@ $(document).ready(function(){
     return false;
   }
 })(jQuery);
-
