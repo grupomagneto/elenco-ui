05142f0d831209a091059c522f937bd9d1d2eb2e
kapcruz <kapcruz@gmail.com>
2017-05-05 17:54:44 -0300

    Update

--- perfil-slim/perfil-html/api/functions.php
+++ perfil-slim/perfil-html/api/functions.php
@@ -3,8 +3,8 @@ require_once ("conecta.php");
 // foreach($_POST["produto"] as $produto) {
 //     mysql_query("INSERT INTO produtos (produtos) VALUES (".$produto.")") or die("Erro  query.<br>Mensagem do servidor: ".mysql_error());
 // }
-	function insereImagemFavorita($conexao, $imagefavorita) {
-	$query = "INSERT INTO perfiltable (imagefavorita) VALUES ('{$imagefavorita}')";
+	function insereImagemFavorita($conexao, $key) {
+	$query = "INSERT INTO perfiltable (imagefavorita) VALUES ('{$key}')";
 
 	return mysqli_query($conexao, $query);
 	}
