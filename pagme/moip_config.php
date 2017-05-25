5d0abffbefac1e4b417527420ee4d1387a0e3542
kapcruz <kapcruz@gmail.com>
2017-05-25 18:53:18 -0300

    Update api

--- pagme/moip_config.php
+++ pagme/moip_config.php
@@ -1,10 +1,21 @@
 <?php
 
-$token = "4LPKLD8JMZPTMSYGU1UTF6DAKJP7OALN";
-$key_token = "FFQZG6GOBHEPPKRGABPNENUEQFYB6WALYMIWRJWI" ;
+include_once "Moip.php";
 
-$auth = $token.':'.$key_token;
+$endpoint = 'https://desenvolvedor.moip.com.br/sandbox';
+$token = '4LPKLD8JMZPTMSYGU1UTF6DAKJP7OALN';
+$key_token = 'FFQZG6GOBHEPPKRGABPNENUEQFYB6WALYMIWRJWI';
 
-$xml = "pagamento";
+$moip = new Moip(new MoipBasicAuth($token, $key_token), $endpoint);
+
+$customer = $moip->customers()->setOwnId('sandbox_v2_1401147277')
+                              ->setFullname(' ')
+                              ->setEmail('sandbox_v2_1401147277@email.com');
+
+$order = $moip->orders()->setOwnId('sandbox_v2_1401147277')
+                        ->addItem('Pedido de testes Sandbox - 1401147277', 1, 'Mais info...', 10000)
+                        ->setShippingAmount(100)
+                        ->setCustomer($customer)
+                        ->create();
 
 ?>
