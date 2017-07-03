85877fe86fb0de99478a0798cd635acce9ee1d8f
kapcruz <kapcruz@gmail.com>
2017-07-03 18:30:03 -0300

    Update mobile landing

--- novoperfil-slim/source/javascripts/all.js
+++ novoperfil-slim/source/javascripts/all.js
@@ -100,41 +100,11 @@ var color2 = "rgb("+r2+","+g2+","+b2+")";
 setInterval(updateGradient,10);
 
 //SLIDE
-$(document).ready(function() {
-
-  $('.slider-carousel').slick({
-    dots: true,
+$(document).on('ready', function() {
+  $(".regular").slick({
+    dots: false,
     infinite: true,
-    speed: 1000,
-    infinite: true,
-    autoplay: true,
-    autoplaySpeed: 3000,
-    centerMode: true,
-    centerPadding: '0px',
     slidesToShow: 3,
-    slidesToScroll: 1,
-    prevArrow: '<span class="btn-prev ion-ios-arrow-left"></span>',
-    nextArrow: '<span class="btn-next ion-ios-arrow-right"></span>',
-    appendArrows: $('.testimonial-slider'),
-    responsive: [{
-        breakpoint: 1024,
-        settings: {
-          slidesToShow: 3,
-        }
-      }, {
-        breakpoint: 600,
-        settings: {
-          slidesToShow: 2,
-        }
-      }, {
-        breakpoint: 480,
-        settings: {
-          slidesToShow: 1,
-        }
-      }
-      // You can unslick at a given breakpoint now by adding:
-      // settings: "unslick"
-      // instead of a settings object
-    ]
+    slidesToScroll: 3
   });
-})
+});
