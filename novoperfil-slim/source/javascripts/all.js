0d21dd0c4e2a2c60c8c8bf03542f852b4674bf70
kapcruz <kapcruz@gmail.com>
2017-07-06 18:03:14 -0300

    Update landing mobile

--- novoperfil-slim/source/javascripts/all.js
+++ novoperfil-slim/source/javascripts/all.js
@@ -136,3 +136,26 @@ $(document).on('ready', function() {
   ]
   });
 });
+
+
+
+var menuTrigger = $('#menu-trigger');
+console.log($('.site-header').outerHeight());
+
+var isToggled = false;
+
+menuTrigger.click(function(e){
+  e.preventDefault();  
+  if(!isToggled){
+   $(this).find('.fa').addClass('rotate-clockwise').removeClass('rotate-counter');
+    console.log("rotate clockwise");
+    isToggled = true;
+    $('#responsive-menu').slideToggle();
+  }else{
+    $(this).find('.fa').addClass('rotate-counter').removeClass('rotate-clockwise');
+    console.log("rotate counterclockwise");
+    $('#responsive-menu').slideToggle();
+    isToggled = false;
+  }
+  
+});
