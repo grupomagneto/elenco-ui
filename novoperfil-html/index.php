42133efc549cc0e723949b2cb33fa08918d96065
kapcruz <kapcruz@gmail.com>
2017-07-07 17:54:23 -0300

    Landing em html
    faltando style menu

---
+++ novoperfil-html/index.php
@@ -0,0 +1,273 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Magneto Elenco</title>
+	<meta content="width=device-width,initial-scale=1.0,minimum-scale=1.0,maximum-scale=1.0,user-scalable=no" name="viewport" >
+	<link rel="stylesheet" href="https://kenwheeler.github.io/slick/slick/slick.css">
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.2.0/css/font-awesome.min.css">
+	<link rel="stylesheet" href="stylesheets/site.css">
+</head>
+<body>
+	<section class="gradient" id="intro">
+
+	  <header class="site-header cf">
+	    <div class="container">
+	      <div class="site-logo">
+	        <img alt="" class="cursor" src="images/logo.svg" />
+	      </div>
+	      <nav class="site-nav">
+	        <ul id="std-menu">
+	          <li>
+	            <a class="active line-right font-family font-normal color-primary cursor" href=""> Entrar</a>
+	          </li>
+	          <li>
+	            <a class="font-family font-normal color-primary cursor" href=""> Cadastrar</a>
+	          </li>
+	        </ul>
+	        <a href="" id="menu-trigger"><i class="fa fa-bars fa-lg"></i></a>
+	      </nav>
+	    </div>
+	  </header>
+
+	  <ul id="responsive-menu">
+	    <li>
+	      <a class="active font-family font-normal color-primary cursor" href=""> Entrar</a>
+	    </li>
+	    <li>
+	      <a class="font-family font-normal color-primary cursor" href=""> Cadastrar</a>
+	    </li>
+	  </ul>
+	  <div class="font-xlarge font-family color-primary bold" id="log"></div>
+	  <div class="container-outline__button">
+	    <button class="button button__medium"><img alt="busca" src="images/busca.svg" /><a class="font-family color-primary" href="">Procuro um perfil</a></button><button class="button button__medium"><img alt="job" src="images/job.svg" /><a class="font-family color-primary" href="">Quero trabalhar</a></button>
+	  </div>
+	  <a class="font-family color-primary font-small text-after" href="">Como funciona?</a><a class="scroll-down cursor" href="#after_gradient"><img alt="" class="cursor" src="images/scroll-down.svg" /></a>
+	</section>
+
+	<section id="after_gradient">
+	  <div class="after_gradient">
+	    <p class="font-family font-large color-primary">
+	      Agência e marketplace de atores e modelos, num só lugar.
+	    </p>
+	  </div>
+	</section>
+
+	<section id="numbers">
+	  <h1 class="font-family font-xlarge color-secondary bold">
+	    Nossos números
+	  </h1>
+	  <div class="numbers">
+	    <div class="number-item">
+	      <img alt="perfil" src="images/img_perfil.svg" />
+	      <p class="number-title font-family font-xlarge color-secondary">
+	        6.671<br /><span class="number-subtitle font-family color-secondary font-small">perfis cadastrados</span>
+	      </p>
+	    </div>
+	    <div class="number-item">
+	      <img alt="cache" src="images/img_cache.svg" />
+	      <p class="number-title number-title__cache font-family font-xlarge color-secondary">
+	        <span class="font-medium">R$</span>1.822.461,<span class="font-medium">33</span><br /><span class="number-subtitle number-subtitle__cache font-family color-secondary font-small">em cachês pagos desde 2009</span>
+	      </p>
+	    </div>
+	    <div class="number-item">
+	      <img alt="job" src="images/img_job.svg" />
+	      <p class="number-title font-family font-xlarge color-secondary">
+	        8.101<br /><span class="number-subtitle font-family color-secondary font-small">trabalhos realizados</span>
+	      </p>
+	    </div>
+	  </div>
+	</section>
+
+	<section id="registration_mode">
+	  <div class="registration_mode">
+	    <h1 class="font-family font-xlarge color-primary bold">
+	      Modalidades de cadastro
+	    </h1>
+	    <p class="font-family font-medium">
+	      Escolha o cadastro ideal para o seu perfil e comece a trabalhar
+	    </p>
+	    <div class="botoes_modalidades">
+	      <button class="button-border_none gratuito"><img src="images/botao-gratuito.svg" /></button><button class="button-border_none premium"><img src="images/botao-premium.svg" /></button><button class="button-border_none profissional"><img src="images/botao-profissional.svg" /></button>
+	    </div>
+	  </div>
+	</section>
+
+	<section id="testimony">
+	  <div class="testimony">
+
+	    <section class="regular slider">
+	      <div>
+	        <div class="testimony-content">
+	          <div class="circle">
+	            <img alt="img-1" src="images/img-1.png" />
+	          </div>
+	          <div class="arroba">
+	            <p class="color-secondary font-medium">
+	              @camila_vilasboa<br /><span class="arroba_since font-family color-secondary font-small">agenciada desde 2010</span>
+	            </p>
+	            <p class="color-secondary italic font-small" id="depoimento">
+	              “Sou Premium e meu investimento já voltou várias vezes em todos esses anos.”
+	            </p>
+	          </div>
+	        </div>
+	      </div>
+	      <div>
+	        <div class="testimony-content">
+	          <div class="circle">
+	            <img alt="img-1" src="images/img-1.png" />
+	          </div>
+	          <div class="arroba">
+	            <p class="color-secondary font-medium">
+	              @camila_vilasboa<br /><span class="arroba_since font-family color-secondary font-small">agenciada desde 2010</span>
+	            </p>
+	            <p class="color-secondary italic font-small" id="depoimento">
+	              “Sou Premium e meu investimento já voltou várias vezes em todos esses anos.”
+	            </p>
+	          </div>
+	        </div>
+	      </div>
+	      <div>
+	        <div class="testimony-content">
+	          <div class="circle">
+	            <img alt="img-2" src="images/img-1.png" />
+	          </div>
+	          <div class="arroba">
+	            <p class="color-secondary font-medium">
+	              @daniel_trindade<br /><span class="arroba_since font-family color-secondary font-small">agenciado desde 2011</span>
+	            </p>
+	            <p class="color-secondary italic font-small" id="depoimento">
+	              “Ganhei meu tempo usando meu celular para conseguir mais trabalhos. Recomendo.”
+	            </p>
+	          </div>
+	        </div>
+	      </div>
+	      <div>
+	        <div class="testimony-content">
+	          <div class="circle">
+	            <img alt="img-3" src="images/img-1.png" />
+	          </div>
+	          <div class="arroba">
+	            <p class="color-secondary font-medium">
+	              @joana_ribeiro<br /><span class="arroba_since font-family color-secondary font-small">agenciada desde 2017</span>
+	            </p>
+	            <p class="color-secondary italic font-small" id="depoimento">
+	              “Agência democrática transparente e ligada no nosso tempo. Minha filha fez o primeiro trabalho e adorou!”
+	            </p>
+	          </div>
+	        </div>
+	      </div>
+	    </section>
+
+	  </div>
+	</section>
+
+	<section id="how">
+	  <div class="how">
+	    <div class="how-title">
+	      <h1 class="color-primary font-xlarge bold">
+	        Como funciona?<br /><span class="how-subtitle color-primary font-normal">para atores, modelos e talentos</span>
+	      </h1>
+	    </div>
+	    <div class="how-detail">
+	      <div class="how-detail_item">
+	        <img alt="user" src="images/user.svg" />
+	        <p class="font-family font-small color-primary">
+	          Cadastre-se
+	        </p>
+	      </div>
+	      <div class="how-detail_item">
+	        <img alt="user" class="arrow-right" src="images/arrow-right.svg" />
+	      </div>
+	      <div class="how-detail_item">
+	        <img alt="job" src="images/job-large.svg" />
+	        <p class="font-family font-small color-primary">
+	          Aceite trabalhos
+	        </p>
+	      </div>
+	      <div class="how-detail_item">
+	        <img alt="user" class="arrow-right" src="images/arrow-right.svg" />
+	      </div>
+	      <div class="how-detail_item">
+	        <img alt="cache" src="images/cache-large.svg" />
+	        <p class="font-family font-small color-primary">
+	          Receba via Magneto
+	        </p>
+	      </div>
+	    </div>
+	    <div class="how-footer">
+	      <h3 class="color-primary font-normal">
+	        Agência e marketplace de atores e modelos, num só lugar.
+	      </h3>
+	    </div>
+	  </div>
+	</section>
+
+	<section id="footer">
+	  <div class="footer-social">
+	    <p class="footer-social__title color-primary font-small">
+	      Redes Sociais
+	    </p>
+	    <nav class="footer-social__img">
+	      <ul>
+	        <li>
+	          <a href="#"><img alt="" src="images/social.svg" /></a>
+	        </li>
+	        <li>
+	          <a href="#"><img alt="" src="images/social.svg" /></a>
+	        </li>
+	        <li>
+	          <a href="#"><img alt="" src="images/social.svg" /></a>
+	        </li>
+	        <li>
+	          <a href="#"><img alt="" src="images/social.svg" /></a>
+	        </li>
+	        <li>
+	          <a href="#"><img alt="" src="images/social.svg" /></a>
+	        </li>
+	      </ul>
+	    </nav>
+	  </div>
+	  <div class="footer-social">
+	    <p class="footer-social__title color-primary font-small">
+	      Agência e Marketplace
+	    </p>
+	    <nav class="footer-social__img footer-sitemap__img">
+	      <ul>
+	        <li>
+	          <a class="color-primary font-small" href="#">Quem somos</a>
+	        </li>
+	        <li>
+	          <a class="color-primary font-small" href="#">Mídia e imprensa</a>
+	        </li>
+	      </ul>
+	    </nav>
+	  </div>
+	  <div class="footer-social">
+	    <p class="footer-social__title color-primary font-small">
+	      Mais informações
+	    </p>
+	    <nav class="footer-social__img footer-info__img">
+	      <ul>
+	        <li>
+	          <a class="color-primary font-small" href="#">Perguntas frequentes / FAQ</a>
+	        </li>
+	        <li>
+	          <a class="color-primary font-small" href="#">Contato</a>
+	        </li>
+	      </ul>
+	    </nav>
+	  </div>
+	</section>
+
+	<div class="last-footer">
+	  <p class="color-primary font-small">
+	    2009-2017 © Magneto Elenco - CNPJ: 10.880.184/0001-85 - SIA Trecho 17 Rua 3 Lote 600 3º andar, Brasília-DF - Termos e Serviços - Política de privacidade
+	  </p>
+	</div>
+
+	<script src="//code.jquery.com/jquery-2.2.4.min.js"></script>
+	<script src="https://kenwheeler.github.io/slick/slick/slick.js"></script>
+	<script src="javascripts/all.js"></script>
+</body>
+</html>
