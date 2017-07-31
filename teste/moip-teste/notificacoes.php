e4b9ba4d0fb153dfd881bac67e5d339e22879627
kapcruz <kapcruz@gmail.com>
2017-07-31 18:24:14 -0300

    Update teste moip

--- teste/moip-teste/notificacoes.php
+++ teste/moip-teste/notificacoes.php
@@ -14,16 +14,19 @@ $key = 'FFQZG6GOBHEPPKRGABPNENUEQFYB6WALYMIWRJWI';
 $moip = new Moip(new BasicAuth($token, $key), Moip::ENDPOINT_SANDBOX);
 
 try {
-    $notification = $moip->notifications()->addEvent('ORDER.WAITING')
-            ->addEvent('PAYMENT.IN_ANALYSIS')
-            ->setTarget('https://requestb.in/18mpc541')
+    $notification = $moip->notifications()->addEvent('ORDER.*')
+            ->addEvent('PAYMENT.WAITING')
+            ->setTarget('https://requestb.in/1lvxtxc1')
+            ->media('WEBHOOK')
             ->create();
     print_r($notification);
 } catch (Exception $e) {
     printf($e->__toString());    
 }
 
-$result = file_get_contents('https://requestb.in/18mpc541');
-   echo $result;
 
+ $result = file_get_contents('https://requestb.in/1lvxtxc1');
+    echo $result;
+
+// url antiga e privada ->setTarget('https://requestb.in/18mpc541')
 ?>
