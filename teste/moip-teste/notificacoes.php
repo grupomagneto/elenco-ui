d59b8ced99323f81f29b5fb7bff00ad601b725c0
kapcruz <kapcruz@gmail.com>
2017-09-25 18:39:52 -0300

    Teste moip webhook

--- teste/moip-teste/notificacoes.php
+++ teste/moip-teste/notificacoes.php
@@ -1,32 +1,79 @@
 <?php 
 
-require 'vendor/autoload.php';
+// require 'vendor/autoload.php';
 
-use Moip\Moip;
-use Moip\Auth\BasicAuth;
-use MoipPayment\Payment;
-use MoipPayment\Order;
-use MoipPayment\Customer;
+// use Moip\Moip;
+// use Moip\Auth\BasicAuth;
+// use MoipPayment\Payment;
+// use MoipPayment\Order;
+// use MoipPayment\Customer;
 
-$token = '4LPKLD8JMZPTMSYGU1UTF6DAKJP7OALN';
-$key = 'FFQZG6GOBHEPPKRGABPNENUEQFYB6WALYMIWRJWI';
+// $token = '4LPKLD8JMZPTMSYGU1UTF6DAKJP7OALN';
+// $key = 'FFQZG6GOBHEPPKRGABPNENUEQFYB6WALYMIWRJWI';
 
-$moip = new Moip(new BasicAuth($token, $key), Moip::ENDPOINT_SANDBOX);
+// $moip = new Moip(new BasicAuth($token, $key), Moip::ENDPOINT_SANDBOX);
 
-try {
-    $notification = $moip->notifications()->addEvent('ORDER.*')
-            ->addEvent('PAYMENT.WAITING')
-            ->setTarget('https://requestb.in/1lvxtxc1')
-            ->media('WEBHOOK')
-            ->create();
-    print_r($notification);
-} catch (Exception $e) {
-    printf($e->__toString());    
-}
+// try {
+//     $notification = $moip->notifications()
+//         ->addEvent('ORDER.*')
+//         ->setTarget("https://".DOMINIO."/notification")
+//         ->create();
+//     print_r($notification);
+// } catch (Exception $e) {
+//     printf($e->__toString());
+// }
+
+// // Pega o RAW data da requisição
+// $json = file_get_contents('php://input');
+// // Converte os dados recebidos
+// $response = json_decode($json, true);
+
+//require 'vendor/autoload.php';
+//include_once "autoload.inc.php";
+
+$header = [];
+$header[] = 'Content-type: application/json';
+
+$header[] = "Authorization: Basic " . base64_encode("4LPKLD8JMZPTMSYGU1UTF6DAKJP7OALN:FFQZG6GOBHEPPKRGABPNENUEQFYB6WALYMIWRJWI");
+ 
+//Monta a URL
+$url = 'https://sandbox.moip.com.br/v2/preferences/notifications' ;
+$curl = curl_init();
+curl_setopt($curl, CURLOPT_URL, $url);
+curl_setopt($curl, CURLOPT_HTTPHEADER, $header);
+curl_setopt($curl, CURLOPT_USERPWD, "4LPKLD8JMZPTMSYGU1UTF6DAKJP7OALN:FFQZG6GOBHEPPKRGABPNENUEQFYB6WALYMIWRJWI");
+curl_setopt($curl, CURLOPT_SSL_VERIFYPEER, false);
+curl_setopt($curl, CURLOPT_USERAGENT, "Mozilla/4.0");
+curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
 
 
- $result = file_get_contents('https://requestb.in/1lvxtxc1');
-    echo $result;
+$ret = curl_exec($curl);
+$err = curl_error($curl);
+curl_close($curl);
 
-// url antiga e privada ->setTarget('https://requestb.in/18mpc541')
+
+echo $ret;
+
+echo '<br />';
+
+$xml = simplexml_load_string($ret);
+echo '<pre>';
+print_r($xml);
+echo '</pre>';
+echo '<br />';
+
+$json = json_encode($xml);
+$array = json_decode($json,TRUE);
+echo '<pre>';
+print_r($array);
+echo '</pre>';
+
+echo '<br />';
+//navega pelos elementos do array, imprimindo cada id
+$id = $array['id']['ORD-432BJUHRZN43'];
+echo $id;
+
+$resourceId = $array['resourceId']['PAY-JVDTVDONUFKG'];
+echo $resourceId;
+
 ?>
