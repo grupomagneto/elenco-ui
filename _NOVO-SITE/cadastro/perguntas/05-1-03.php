989a8295ec685630764359728f84d5edf8f409e0
vini-goulart <vinigoulart@iMac-de-iMac.local>
2018-01-24 07:19:12 -0200

    Video de Instruções

--- _NOVO-SITE/cadastro/perguntas/05-1-03.php
+++ _NOVO-SITE/cadastro/perguntas/05-1-03.php
@@ -1,21 +1,22 @@
 <div class='conteudo flexbox wrap text-align-center space-between-vertical justify-center'>
     <div class='conteudo flexbox wrap text-align-center space-between-vertical justify-center container-video'>
         <div class='titulo heavy white large'>
-        <!-- Como fazer bons autorretratos -->
-        Autorretratos no padrão Magneto Elenco
+        <!-- Como fazer bons autorretratos: -->
+        <!-- Autorretratos no padrão Magneto Elenco -->
         </div>
-        <div class='subtitulo avenir white text-align-left'>
-            <!-- Assista ao vídeo explicativo antes de fazer suas fotos
-            <div class='video'>
-                <img src="../_images/video.svg" style='width:100%' />
-            </div> -->
-            <span class="heavy">Siga as instruções para fazer suas fotos:</span>
+        <div class='subtitulo avenir white text-align-center'>
+            Assista ao vídeo explicativo antes de fazer suas fotos:
+            <div class='video frame'>
+                <!-- <img src="../_images/video_fotos.mp4" style='width:100%' /> -->
+                <video height='100%' controls><source src="../_images/video_autorretratos.mp4" type="video/mp4"></video>
+            </div>
+            <!-- <span class="heavy">Siga as instruções para fazer suas fotos:</span>
                 <ul class="ul_lista_selfies">
                     <li class="li_lista_selfies">Use um muro branco ao ar livre como fundo para suas fotos;</li>
                     <li class="li_lista_selfies">Faça suas fotos durante o dia, na sombra e desligue o flash;</li>
                     <li class="li_lista_selfies">Estique bem os braços e olhe para para câmera do celular, não olhe para a tela;</li>
                     <li class="li_lista_selfies">Pele limpa, cabelo penteado e maquiagem leve ajudam muito;</li>
-                </ul>
+                </ul> -->
         </div>
         <div class='botoes'>
             <button id='btn_video' class='botao'>Continuar</button>
